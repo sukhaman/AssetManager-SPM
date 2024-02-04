95211646cee0984a584c02ae6c4a680a3c75d4fc
Sukhaman Singh <sukhamandev@gmail.com>
2024-02-04 09:04:00 -0500

    first commit

---
+++ Sources/AssetManager/AssetManager.xcframework/ios-arm64_x86_64-simulator/AssetManager.framework/Headers/AssetManager.h
@@ -0,0 +1,18 @@
+//
+//  AssetManager.h
+//  AssetManager
+//
+//  Created by Sukhaman Singh on 2/4/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AssetManager.
+FOUNDATION_EXPORT double AssetManagerVersionNumber;
+
+//! Project version string for AssetManager.
+FOUNDATION_EXPORT const unsigned char AssetManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AssetManager/PublicHeader.h>
+
+
